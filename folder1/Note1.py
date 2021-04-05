df86069fea142282c7f63b150b866cd73d65d452
anindita-Databricks <anindita-Databricks>
2021-04-05 20:22:26 +0000

    first
    desc

---
+++ folder1/Note1.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+print("Note1")
+
+# COMMAND ----------
+
+
---
+++ test_ML.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %sh
+# MAGIC python -V
+
+# COMMAND ----------
+
+import os
+print("Hello World")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC val first = "Hello"
+# MAGIC val second = "World"
+# MAGIC print(first, second)
+
+# COMMAND ----------
+
+
